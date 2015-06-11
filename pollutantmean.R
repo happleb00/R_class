80b511e993dc735dc509a38707cc3009ec480133
Katja Seltmann <moon@begoniasociety.org>
2015-06-11 15:28:48 -0400

    new comment in R file

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,3 +1,5 @@
+##need to add text about the file here
+
 pollutantmean <- function(directory, pollutant, id = 1:332) {
 	files_list <- list.files(directory, full.names = TRUE)
 	dat <- data.frame()
