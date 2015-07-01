d6d56b20da22ecc03900a339d713a6018a2c4826
Katja Seltmann <moon@begoniasociety.org>
2015-07-01 10:35:36 -0400

    2 versions of loop comparisons

--- database_assignment.R
+++ database_assignment.R
@@ -1,43 +1,28 @@
 mexlocfull <- read.table("PBI_Mexico_data.txt", header = TRUE, sep = "\t" ,fill = TRUE, stringsAsFactors = FALSE)
 
 
-##data frame of relevant Mexico locality data:
-mexloc <- mexlocfull[,c(4:9)]
-head(mexloc)
-
-
-## how to subset mexloc Localities - not sure if I will use this for anything yet:
-#mexlocsub <- subset(mexloc, select = Locality)
-#summary(mexlocsub)
-head(mexlocsub)
-
 ###########TODO - subset based on a STate_Prov
 
-## prints TRUE or FALSE for each row.
-## TRUE - duplicate locality
-## FALSE - there is no duplicate or the first of a set of duplicate localities is read.
-dupe <- duplicated(mexloc)
-head(dupe)
 
-## list of unique duplicated localities
-unqloc <- unique(mexloc, nmax=1)
-#unqloc <- unique(mexloc[duplicated(mexloc),]) also works
-#summary(unqloc)
-head(unqloc)
+#Version 1: Finds a series of localities and compares to itself, adds if dup to dataframe
+#=======================================
 
+dupelist <- unique(mexloc$Locality)
+head(dupelist)
+for (i in dupelist){
+  listfiles <- mexloc$Locality
+  head(listfiles)
+  somevariable <-  ifelse(i in listfiles > 1, TRUE, FALSE) 
+  }
 
+  somevariable
 
-for (i in 1:300){
-  listfiles <- mexloc[i,]
-  dupelist <- unique(mexloc[i,], nmax=1)
-  return <-  ifelse(listfiles==dupelist, print(i), print("no")) ##my idea was to see if I am able to find the duplicates then print a UID # to locate them
-}
-#head(listfiles) this is only printing the last value(300)
-#head(dupelist) also only printing the last value(300)
-return
+#TODO add cbind to mexloc
+help(return)
+#=======================================
 
 
-#Finds a series of localities and compares to itself, adds if dup to dataframe
+#Version 2: Finds a series of localities and compares to itself, adds if dup to dataframe
 #=======================================
 mexloc <- unique(mexlocfull[,c(4:9)])
 mexloc.locality <- subset(mexloc, select = Locality) #same as mexloc$locality?
@@ -46,13 +31,16 @@ head(mexloc)
 nrow(mexloc)
 
 for (i in mexloc){
-  dupe <- duplicated(i) #how to make this test more fuzzy? #how to compare 2 different files?
+  dupe <- duplicated(i) #TODO: how to make this test more fuzzy? 
 }
 mexdup <- cbind(mexloc, dupe)
 head(mexdup)
 #=======================================
 
 
+
+
+###########====working space below---older code
 help(subset)
 
 
