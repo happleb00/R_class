02443a473324fe3d3ab9689842229423bda30951
Katja Seltmann <moon@begoniasociety.org>
2015-07-01 08:20:20 -0400

    katja edits

--- database_assignment.R
+++ database_assignment.R
@@ -3,25 +3,27 @@ mexlocfull <- read.table("PBI_Mexico_data.txt", header = TRUE, sep = "\t" ,fill 
 
 ##data frame of relevant Mexico locality data:
 mexloc <- mexlocfull[,c(4:9)]
-#head(mexloc)
+head(mexloc)
 
 
 ## how to subset mexloc Localities - not sure if I will use this for anything yet:
 #mexlocsub <- subset(mexloc, select = Locality)
 #summary(mexlocsub)
-#head(mexlocsub)
+head(mexlocsub)
+
+###########TODO - subset based on a STate_Prov
 
 ## prints TRUE or FALSE for each row.
 ## TRUE - duplicate locality
 ## FALSE - there is no duplicate or the first of a set of duplicate localities is read.
 dupe <- duplicated(mexloc)
-#head(dupe)
+head(dupe)
 
 ## list of unique duplicated localities
 unqloc <- unique(mexloc, nmax=1)
 #unqloc <- unique(mexloc[duplicated(mexloc),]) also works
 #summary(unqloc)
-#head(unqloc)
+head(unqloc)
 
 
 
@@ -35,7 +37,23 @@ for (i in 1:300){
 return
 
 
-help(unique)
+#Finds a series of localities and compares to itself, adds if dup to dataframe
+#=======================================
+mexloc <- unique(mexlocfull[,c(4:9)])
+mexloc.locality <- subset(mexloc, select = Locality) #same as mexloc$locality?
+
+head(mexloc)
+nrow(mexloc)
+
+for (i in mexloc){
+  dupe <- duplicated(i) #how to make this test more fuzzy? #how to compare 2 different files?
+}
+mexdup <- cbind(mexloc, dupe)
+head(mexdup)
+#=======================================
+
+
+help(subset)
 help("duplicated")
 help("all.equal")
 help("Compare")
